e560900c4d5637e41a158d05a192d680be61ef03
Diego Buergin <d.buergin@beh.ch>
2020-09-09 14:36:53 +0200

    added VS2019 solution and project

---
+++ src/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.FlightSimulator.SimConnect;
+
+namespace SimConnectManagedTest
+{
+    static class Program
+    {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        private static SimConnect simConnect;
+
+        const int WM_USER_SIMCONNECT = 0x0402;
+
+        public enum Events
+        {
+            EventSteerAileron
+        }
+        enum NotificationGroups
+        {
+            Group0,
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            simConnect = new SimConnect("SimConnectTest", GetConsoleWindow(), WM_USER_SIMCONNECT, null, 0);
+
+            simConnect.MapClientEventToSimEvent(Events.EventSteerAileron, "AILERON_SET");
+            simConnect.AddClientEventToNotificationGroup(NotificationGroups.Group0, Events.EventSteerAileron, false);
+
+            simConnect.SetNotificationGroupPriority(NotificationGroups.Group0, SimConnect.SIMCONNECT_GROUP_PRIORITY_HIGHEST);
+
+            while (true)
+            {
+                Thread.Sleep(50);
+                int d = (int)(Math.Sin((DateTime.Now - DateTime.Today).TotalSeconds / 2) * 16000);
+                simConnect.TransmitClientEvent(0, Events.EventSteerAileron, (uint)d, NotificationGroups.Group0, SIMCONNECT_EVENT_FLAG.GROUPID_IS_PRIORITY);
+            }
+        }
+    }
+}
